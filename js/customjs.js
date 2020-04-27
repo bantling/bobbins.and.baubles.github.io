5db2219ddd4bc4028c672631bb5625ba928e7349
Greg Hall <greghall@bantling.me>
2020-04-27 19:46:41 -0300

    Initial Commit

---
+++ js/customjs.js
@@ -0,0 +1,122 @@
+﻿
+/*====================================================
+                        SERVICES
+====================================================*/
+$(function () {
+
+    // animate on scroll
+    new WOW().init();
+});
+
+
+
+/* Show and hide menu */
+
+// Show/Hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("top-nav");
+            $("#back-to-top").fadeOut();
+
+        } else {
+            // show nav
+            $("nav").addClass("top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+// highlight menu item on scroll
+$(document).ready(function() {
+    
+    'use strict';
+    
+    $(window).scroll( function() {
+        
+        'use strict';
+        
+        $("section").each(function()  {
+            
+            'use strict';
+            
+            var bb = $(this).attr("id");  // ABOUT, 
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+            
+            if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+             
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+           
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+                
+            }
+            
+            
+        });
+    
+    
+    });
+    
+    
+});
+
+
+
+
+// Images
+
+
+// Add bx slider to screens
+$(document).ready(function() {
+    
+    $('.bxslider').bxSlider({
+        
+        slideWidth: 282.5,
+        auto: true,
+        minSlides: 1, 
+        maxSlides: 4, 
+        slideMargin: 30
+    });
+    
+});
+
+
+
+// Add animation/ Initialize Woo
+$(document).ready(function() {
+    
+    'use strict';
+   
+    new WOW().init();
+    
+});
+
+
+// back-to-top button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
